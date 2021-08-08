a3bdb14560a47f5c8d7885e8aa6badf364fe21d0
ljh97820 <ljh97820@naver.com>
2021-08-08 17:33:36 +0900

    Progress Bar가 제대로 동작하지 않던 문제 fix 제일 처음 START 버튼 추가

--- PopQuiz/Controllers/RootController.swift
+++ PopQuiz/Controllers/RootController.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 class RootController: UIViewController {
-    let one: String = "애니메이션 '이웃집 토토로'에서 \n토토로는 어른에게는 보이지 않는다." //YES
-    let two: String = "달걀은 어린 닭이 낳은 것일수록 \n그 크기가 크다." //NO
-    let three: String = "무리한 다이어트는 생리불순 및 \n골다공증을 유발할 수 있다." //YES
-    let four: String = "꺼벙이란 꿩의 새끼를 뜻한다." //YES
-    let five: String = "인질이 인질범들에게 동화되어 \n경찰들을 적대시하는 현상을 가리켜 \n스톡홀름 증후군이라고 한다." //YES
-    let six: String = "물고기도 기침을 한다." //YES
-    let seven: String = "TV를 가까이서 보면 시력이 나빠진다." //NO
-    let eight: String = "닭도 왼발잡이, 오른발잡이가 있다." //YES
-    let nine: String = "만화 '검정 고무신'의 배경시대는 1990년대이다." //YES
-    let ten: String = "다이아몬드는 고온의 열에도 타지 않는다." //NO
+//    let one: String = "애니메이션 '이웃집 토토로'에서 \n토토로는 어른에게는 보이지 않는다." //YES
+//    let two: String = "달걀은 어린 닭이 낳은 것일수록 \n그 크기가 크다." //NO
+//    let three: String = "무리한 다이어트는 생리불순 및 \n골다공증을 유발할 수 있다." //YES
+//    let four: String = "꺼벙이란 꿩의 새끼를 뜻한다." //YES
+//    let five: String = "인질이 인질범들에게 동화되어 \n경찰들을 적대시하는 현상을 가리켜 \n스톡홀름 증후군이라고 한다." //YES
+//    let six: String = "물고기도 기침을 한다." //YES
+//    let seven: String = "TV를 가까이서 보면 시력이 나빠진다." //NO
+//    let eight: String = "닭도 왼발잡이, 오른발잡이가 있다." //YES
+//    let nine: String = "만화 '검정 고무신'의 배경시대는 1990년대이다." //YES
+//    let ten: String = "다이아몬드는 고온의 열에도 타지 않는다." //NO
     
     let arr: [String?] = ["애니메이션 '이웃집 토토로'에서 \n토토로는 어른에게는 보이지 않는다.", "달걀은 어린 닭이 낳은 것일수록 \n그 크기가 크다.",
                          "무리한 다이어트는 생리불순 및 \n골다공증을 유발할 수 있다.", "꺼벙이란 꿩의 새끼를 뜻한다.",
@@ -26,10 +26,22 @@ class RootController: UIViewController {
                          "다이아몬드는 고온의 열에도 타지 않는다."]
     var index = 0
     
+    lazy var startButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("START", for: UIControl.State.normal)
+        button.backgroundColor = .systemIndigo
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 28, weight: UIFont.Weight.bold)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 6
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        button.addTarget(self, action: #selector(startButtonTapped), for: UIControl.Event.touchUpInside)
+        return button
+    }()
+    
     // MARK: Properties
     lazy var question: UILabel = {
         let label = UILabel()
-        label.text = arr[0]
         label.textAlignment = .center
         label.textColor = UIColor.white
         label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.heavy)
@@ -63,16 +75,43 @@ class RootController: UIViewController {
         return button
     }()
     
+    lazy var progressView: UIProgressView = {
+        let pv = UIProgressView()
+        pv.progressViewStyle = .default
+        pv.progressTintColor = .green
+        pv.trackTintColor = .lightGray
+//        pv.progress = 0.0
+        pv.layer.cornerRadius = 50
+        pv.setProgress(0.0, animated: true)
+        return pv
+    }()
+    
+    
+    
     // MARK: Selectors
+    @objc func startButtonTapped() {
+        startButton.layer.isHidden = true
+        startButton.isEnabled = false
+        
+        yesButtonTapped()
+        progressView.progress = 0.0
+    }
+    
     @objc func yesButtonTapped() {
         
         if index <= 9 {
             question.text = arr[index]
             index = index + 1
+            progressView.progress += 0.1
         } else if index > 9 {
             index = 0
             question.text = arr[index]
             index = index + 1
+            progressView.progress += 0.1
+        }
+        
+        if progressView.progress == 1 {
+            progressView.progress = 0.0
         }
     }
     
@@ -81,10 +120,16 @@ class RootController: UIViewController {
         if index <= 9 {
             question.text = arr[index]
             index = index + 1
+            progressView.progress += 0.1
         } else if index > 9 {
             index = 0
             question.text = arr[index]
             index = index + 1
+            progressView.progress += 0.1
+        }
+        
+        if progressView.progress == 1 {
+            progressView.progress = 0.0
         }
     }
     
@@ -98,6 +143,14 @@ class RootController: UIViewController {
     // MARK: Configure
     func configureViewComponent() {
         view.backgroundColor = .systemIndigo
+        
+        view.addSubview(startButton)
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        startButton.widthAnchor.constraint(equalToConstant: 350).isActive = true
+        startButton.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        
         
         view.addSubview(question)
         question.translatesAutoresizingMaskIntoConstraints = false
@@ -117,5 +170,12 @@ class RootController: UIViewController {
         noButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         noButton.widthAnchor.constraint(equalToConstant: view.frame.width - 235).isActive = true
         noButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        view.addSubview(progressView)
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        progressView.topAnchor.constraint(equalTo: yesButton.bottomAnchor, constant: 15).isActive = true
+        progressView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        progressView.widthAnchor.constraint(equalToConstant: view.frame.width - 50).isActive = true
+        progressView.heightAnchor.constraint(equalToConstant: 10).isActive = true
     }
 }
