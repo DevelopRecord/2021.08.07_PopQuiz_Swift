5197ea2c2289b80f76ac7f12515602e10b4157b4
ljh97820 <ljh97820@naver.com>
2021-08-08 16:16:42 +0900

    2021. 8. 8 문제 구성

--- PopQuiz/Controllers/RootController.swift
+++ PopQuiz/Controllers/RootController.swift
@@ -8,96 +8,74 @@
 import UIKit
 
 class RootController: UIViewController {
+    let one: String = "애니메이션 '이웃집 토토로'에서 \n토토로는 어른에게는 보이지 않는다." //YES
+    let two: String = "달걀은 어린 닭이 낳은 것일수록 \n그 크기가 크다." //NO
+    let three: String = "무리한 다이어트는 생리불순 및 \n골다공증을 유발할 수 있다." //YES
+    let four: String = "꺼벙이란 꿩의 새끼를 뜻한다." //YES
+    let five: String = "인질이 인질범들에게 동화되어 \n경찰들을 적대시하는 현상을 가리켜 \n스톡홀름 증후군이라고 한다." //YES
+    let six: String = "물고기도 기침을 한다." //YES
+    let seven: String = "TV를 가까이서 보면 시력이 나빠진다." //NO
+    let eight: String = "닭도 왼발잡이, 오른발잡이가 있다." //YES
+    let nine: String = "만화 '검정 고무신'의 배경시대는 1990년대이다." //YES
+    let ten: String = "다이아몬드는 고온의 열에도 타지 않는다." //NO
+    
+    let arr: [String?] = ["애니메이션 '이웃집 토토로'에서 \n토토로는 어른에게는 보이지 않는다.", "달걀은 어린 닭이 낳은 것일수록 \n그 크기가 크다.",
+                         "무리한 다이어트는 생리불순 및 \n골다공증을 유발할 수 있다.", "꺼벙이란 꿩의 새끼를 뜻한다.",
+                         "인질이 인질범들에게 동화되어 \n경찰들을 적대시하는 현상을 가리켜 \n스톡홀름 증후군이라고 한다.", "물고기도 기침을 한다.",
+                         "TV를 가까이서 보면 시력이 나빠진다.", "닭도 왼발잡이, 오른발잡이가 있다.", "만화 '검정 고무신'의 배경시대는 1990년대이다.",
+                         "다이아몬드는 고온의 열에도 타지 않는다."]
     
     // MARK: Properties
     lazy var question: UILabel = {
         let label = UILabel()
-        return label
-    }()
-    
-    lazy var quizOne: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizTwo: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizThree: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizFour: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizFive: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizSix: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizSeven: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizEight: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizNine: UILabel = {
-        let label = UILabel()
-        
-        return label
-    }()
-    
-    lazy var quizTen: UILabel = {
-        let label = UILabel()
-        
+        label.text = arr[0]
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 24, weight: UIFont.Weight.heavy)
+        label.numberOfLines = 2
         return label
     }()
     
     lazy var yesButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: UIButton.ButtonType.system)
         button.setTitle("YES", for: UIControl.State.normal)
-        button.backgroundColor = .systemBlue
-        button.titleLabel?.font = .systemFont(ofSize: 22)
+        button.backgroundColor = .systemIndigo
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
         button.layer.cornerRadius = 10
-        button.layer.borderWidth = 1
+        button.layer.borderWidth = 6
         button.layer.borderColor = UIColor.systemBlue.cgColor
-//        button.addTarget(self, action: #selector(calculateButtonTapped), for: UIControl.Event.touchUpInside)
+        button.addTarget(self, action: #selector(yesButtonTapped), for: UIControl.Event.touchUpInside)
         return button
     }()
     
     lazy var noButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: UIButton.ButtonType.system)
         button.setTitle("NO", for: UIControl.State.normal)
-        button.backgroundColor = .systemBlue
-        button.titleLabel?.font = .systemFont(ofSize: 22)
+        button.backgroundColor = .systemIndigo
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 22, weight: UIFont.Weight.bold)
+        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
         button.layer.cornerRadius = 10
-        button.layer.borderWidth = 1
+        button.layer.borderWidth = 6
         button.layer.borderColor = UIColor.systemBlue.cgColor
-//        button.addTarget(self, action: #selector(calculateButtonTapped), for: UIControl.Event.touchUpInside)
+        button.addTarget(self, action: #selector(noButtonTapped), for: UIControl.Event.touchUpInside)
         return button
     }()
+    
+    // MARK: Selectors
+    @objc func yesButtonTapped(_ sender: UIButton) {
+        
+        if sender.isSelected {
+            question.text = "\(arr[0...9])"
+        }
+        
+    }
+    
+    @objc func noButtonTapped() {
+        print("nobtn~~@@")
+        
+        question.text = two
+    }
     
     // MARK: Init
     override func viewDidLoad() {
@@ -108,9 +86,12 @@ class RootController: UIViewController {
     
     // MARK: Configure
     func configureViewComponent() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemIndigo
         
-        view.addSubview(quizOne)
+        view.addSubview(question)
+        question.translatesAutoresizingMaskIntoConstraints = false
+        question.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        question.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
         
         view.addSubview(yesButton)
         yesButton.translatesAutoresizingMaskIntoConstraints = false
